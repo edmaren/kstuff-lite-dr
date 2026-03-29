f1a47f7dfe0bb924e40bd4a4b8cfcc92a3b7340a
Roman Tarasov <i@drakmor.ru>
2026-03-29 11:18:48 +0300

    Bump version to 1.1-dr stable

--- ps5-kstuff/main.c
+++ ps5-kstuff/main.c
@@ -963,7 +963,7 @@ int main(void* ds, int a, int b, uintptr_t c, uintptr_t d)
 
     // Header
     const char *hdr =
-        "Welcome To Kstuff Lite 1.1-dr-beta6\nPlayStation 5 FW: ";
+        "Welcome To Kstuff Lite 1.1-dr\nPlayStation 5 FW: ";
     while (*hdr) *p++ = *hdr++;
 
     // Major
